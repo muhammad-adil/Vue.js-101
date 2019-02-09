e56e9d98857baf3b78628592a024cd2bd23cd3c8
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-02-09 20:21:10 +0500

    vue skills todo app initialize

---
+++ 01-vue-skills/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Skills/>
+  </div>
+</template>
+
+<script>
+import Skills from "./components/Skills.vue";
+
+export default {
+  name: "app",
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
