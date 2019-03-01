fa9debcf932d3fc86437160f94a64e4a3466a317
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-03-01 11:21:10 +0500

    Basic vue code practice

---
+++ 03-vue-basics/src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="card">
+    <header>
+      <strong>{{ title }}</strong>
+    </header>
+    <p>{{ content }}</p>
+    <div v-html="contentWithHtml"></div>
+    <ul>
+      <li v-for="(item, index) in items">
+        <button v-on:click="deleteItem(index)">X</button>
+        {{ item.text }}
+      </li>
+    </ul>
+    <br />
+    <input id="itemForm" v-on:keypress.enter="addItem" />
+    <button v-on:click="addItem">Add Anything in list</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "Vue-basics",
+      content: "Anything can come here....",
+      contentWithHtml:
+        "I am with html <strong>tags</strong>, Anything <i>can</i>....",
+      items: [
+        { text: "Card 1" },
+        { text: "Card 2" },
+        { text: "Card 3" },
+        { text: "Card 4" }
+      ]
+    };
+  },
+  methods: {
+    addItem: function() {
+      var input = document.getElementById("itemForm");
+      if (input.value !== "") {
+        this.items.push({
+          text: input.value
+        });
+      }
+      input.value = "";
+    },
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#card ul li {
+  list-style-type: none;
+}
+</style>
---
+++ 03-vue-basics/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Card />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    Card
+  }
+};
+</script>
