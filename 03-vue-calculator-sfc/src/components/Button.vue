1cf1d8f1b0011f613223a3fe3cc1122f2b73335b
Muhammad-Adil <adil.muhammad93@gmail.com>
2020-12-06 14:21:10 +0500

    03 Vue calculator

---
+++ 03-vue-calculator-sfc/src/components/Button.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="vuebutton">
+      <button class="calc-button" 
+              v-on:click=buttonPressed
+              v-bind:class="{opButton: bValues.isOp, regButton: bValues.isReg}">
+               {{bValues.name}}
+      </button>
+  </div>
+</template>
+
+<script>
+import Velocity from "velocity-animate";
+export default {
+  name: 'vuebutton',
+  props: {
+      bValues: Object
+  },
+  methods:{
+      buttonPressed: function(event){
+          this.$emit('handle-press', event.target);
+          let bt = event.target;
+          Velocity(bt, 
+                  {"font-size": "40px", opacity: 0.7},
+                  {duration: 100, 
+                   easing: "bounceIn",
+                   complete: function(){
+                    Velocity(bt, 
+                    {"font-size": "30px", opacity: 1},
+                    {duration: 100, easing: "bounceOut"});
+                  }}
+                  );
+          
+      }
+  }
+}
+</script>
+
+<style>
+#vuebutton {
+  margin: 0px;
+  height: 100px;
+  box-sizing: border-box;
+  float: left;
+}
+.calc-button{
+    width: 100%;
+    height: 100%;
+    font-family: Lato, Verdana, sans-serif;
+    text-align: center;
+    font-size: 30px;
+    color: white;
+}
+.opButton{
+    background-color: #38908f;
+}
+.regButton{
+    background-color: #676767;
+}
+</style>
---
+++ 03-vue-calculator-sfc/src/components/Display.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="display">
+      <div id="displayText">
+        <div id = "leftOf">{{displayValue}}</div>
+        <div id = "blinker"> <Blinker /> </div>
+      </div>
+      <div id="subText"><p>{{subDisplayValue}}</p></div>
+  </div>
+</template>
+
+<script>
+import Blinker from "./Blinker.vue";
+import Velocity from "velocity-animate";
+export default {
+  name: 'display',
+  props: {
+      displayValue: String,
+      subDisplayValue: String
+  },
+  components:{
+    Blinker
+  },
+  mounted:function(){
+    this.onLoady()
+  },
+  methods: {
+    onLoady: function() {
+      Velocity(document.getElementById("blinker"), {opacity: 0}, {loop: true});
+    }
+  }
+  
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+#display {
+  width: 100%;
+  border-radius: 5px;
+  height: 200px;
+  float: right;
+}
+#leftOf{
+  width: 99.75%;
+}
+#blinker{
+  width: 1.00%;
+}
+#displayText,  #subText{
+    font-family: 'Lato', sans-serif;
+    text-align: right;
+    margin-bottom: 0px;
+    overflow:hidden;
+    text-overflow: ellipsis;
+}
+#displayText{
+    display: flex;
+    flex-direction: row;
+    height: 100px;
+    font-size: 3em;
+}
+#subText{
+    height: 100px;
+    font-size: 1.5em;
+    color: #38908f;
+    border: none;
+    background-color: #f9f9f9;
+}
+</style>
